a721f9b2c34d173d382049bf6be81d49c3809744
XbozzX <muhammad_19000649@utp.edu.my>
2021-11-16 12:01:43 +0800

    Create datalist

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,84 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MaterialApp(
+  home: ContactList(),
+));
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class ContactList extends StatefulWidget {
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _ContactListState createState() => _ContactListState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ContactListState extends State<ContactList> {
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  List<String> contactName = [
+    "Chan Saw Lin", " Lee Saw Loy", "Khaw Tong Lin", "Lim Kok Lin"
+    , "Low Jun Wei", "Yong Weng Kai", "Jayden Lee", "Kong Kah Yan"
+    , "Jasmine Lau", "Chan Saw Lin"
+  ];
+
+  List<int> contactNumber = [
+    0152131113, 0161231346, 0158398109, 0168279101, 0112731912
+    , 0172332743, 0191236439, 0111931233, 0162879190, 016783239
+  ];
+
+  List<String> contactCheckIn = [
+    "2020-06-30 16:10:05", "2020-07-11 15:39:59", "2020-08-19 11:10:18"
+    , "2020-08-19 11:11:35", "2020-08-15 13:00:05", "2020-07-31 18:10:11"
+    , "2020-08-22 08:10:38", "2020-07-11 12:00:00", "2020-08-01 12:10:05"
+    , "2020-08-23 11:59:05"
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.blueGrey[200],
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Contact'),
+        backgroundColor: Colors.amber,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+       body: Container(
+         padding: EdgeInsets.all(20),
+         child: Column(
+           children: <Widget>[
+             buildListName(), buildListNumber()
+           ],
+         ),
+       )
+      );
   }
+
+  // create the contact name
+  Widget buildListName() => ListView.builder(
+    shrinkWrap: true, // shrinkWrap function to make the auto size
+    itemCount: contactName.length,
+    itemBuilder: (context, index) {
+      final name = contactName[index];
+    
+      return buildItemName(name);
+    },
+  );
+
+  // create contact number 
+  Widget buildListNumber() => ListView.builder(
+    shrinkWrap: true, // shrinkWrap function to make the auto size
+    itemCount: contactNumber.length, 
+    itemBuilder: (context, index) {
+      final number = contactNumber[index];
+  
+      return buildItemNumber(number);
+    },
+  );
+
+  Widget buildItemName(String name) => ListTile(
+    title: Text('Name : $name '),
+
+    
+  );
+
+  Widget buildItemNumber(int number) => ListTile(
+    title: Text('Phone : $number '),
+  );
+
 }
